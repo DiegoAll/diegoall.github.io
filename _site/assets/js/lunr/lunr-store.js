b095bae6b2d308480acf006cb05329c1540a34df
DiegoAll <dposadallano@gmail.com>
2024-10-30 23:16:00 -0500

    Falco runtime security structure

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -24,7 +24,7 @@ var store = [{
         "teaser": "/assets/images/htb-writeup-delivery/delivery_logo.png"
       },{
         "title": "Falco runtime security",
-        "excerpt":"Buenas noches parcero Panita Nmap scan report for 10.129.148.141 Host is up (0.018s latency). Not shown: 65532 closed ports PORT STATE SERVICE VERSION 22/tcp open ssh OpenSSH 7.9p1 Debian 10+deb10u2 (protocol 2.0) | ssh-hostkey: | 2048 9c:40:fa:85:9b:01:ac:ac:0e:bc:0c:19:51:8a:ee:27 (RSA) | 256 5a:0c:c0:3b:9b:76:55:2e:6e:c4:f4:b9:5d:76:17:09 (ECDSA) |_ 256 b7:9d:f7:48:9d:a2:f2:76:30:fd:42:d3:35:3a:80:8c (ED25519) 80/tcp open http nginx...","categories": ["cyber","kubernetes"],
+        "excerpt":"Intro ¿Qué es Falco? Falco es un proyecto creado por sysdig en 2016. Monitoreo de seguridad en runtime security de contenedores Evalua syscalls del contenedor al kernel a través de reglas predefinidas y/o customizadas ¿Por qué es importante runtime security? ola ¿Qué detecta Falco? What Falco checks for? ¿Cómo funciona...","categories": ["cyber","kubernetes"],
         "tags": ["falco","runtime security","syscall","threat detection","rules"],
         "url": "/cyber/kubernetes/falco-runtime-security/",
         "teaser": "/assets/images/htb-writeup-delivery/delivery_logo.png"
